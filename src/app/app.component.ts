dae5b323d64cc1ba6a6b3e4e332a83526d49e992
semika <semika.siriwardana@shipxpress.com>
2017-02-06 12:39:41 +0530

    Initial Import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { Component} from '@angular/core';
+
+@Component( {
+    moduleId: module.id,
+    selector : 'app-root',
+    templateUrl : './app.component.html',
+    styleUrls : ['./app.component.css'] 
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = "Tour of Heroes";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+// Application components
 import { AppComponent } from './app.component';
+import { HeroDetailComponent} from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+//Application services
+import { HeroService} from './hero.service';
+
+//Application modules
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // component goes here...
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  imports: [
+  imports: [ //Angular modules goes here.. 
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule 
   ],
-  providers: [],
+  providers: [HeroService], ///services goes here.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
